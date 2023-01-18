244db061727289b08737a11d8e1391f308364f70
Gage <yaujin@protonmail.com>
2023-01-17 19:42:46 -0600

    made query selector and event listener to show or hide form on click

---
+++ javascript.js
@@ -0,0 +1,10 @@
+const newBookBtn = document.querySelector('.new-book');
+
+newBookBtn.addEventListener('click', () => {
+  const form = document.querySelector('form');
+  if (form.style.display === 'none') {
+    form.style.display = 'flex';
+  } else {
+    form.style.display = 'none';
+  }
+});
