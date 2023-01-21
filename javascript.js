44daa84daca839ee2e374c28094dc1d1465f05e5
Gage <yaujin@protonmail.com>
2023-01-21 15:35:07 -0600

    made the addBooksToTable function to be called later when user submits form with new book

--- javascript.js
+++ javascript.js
@@ -9,7 +9,6 @@ function Book(title, author, pages, status) {
 
 const dune = new Book('Dune', 'some', 500, 'read');
 const lordOfTheRings = new Book('lotr', 'tolkien', 400, 'not read');
-
 myLibrary.push(dune, lordOfTheRings);
 
 const newBookBtn = document.querySelector('.new-book');
@@ -22,3 +21,20 @@ newBookBtn.addEventListener('click', () => {
     form.style.display = 'none';
   }
 });
+
+const tbody = document.querySelector('tbody');
+
+function addBooksToTable() {
+  const i = myLibrary.length - 1;
+  const currentBook = myLibrary[i];
+  const tr = document.createElement('tr');
+
+  const tableBookData = (item) => {
+    const td = document.createElement('td');
+    tr.appendChild(td);
+    td.appendChild(document.createTextNode(item));
+    tbody.appendChild(tr);
+  };
+  Object.values(currentBook).forEach(tableBookData);
+  tr.appendChild(document.createElement('button'));
+}
