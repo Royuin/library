c50b6b42fa37a3e30089131c7d9862466febc81b
Gage <yaujin@protonmail.com>
2023-01-20 18:16:14 -0600

    made Book constructor to be utilized when user adds book info to form

--- javascript.js
+++ javascript.js
@@ -1,5 +1,12 @@
 const myLibrary = [];
 
+function Book(title, author, pages, status) {
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.status = status;
+}
+
 const newBookBtn = document.querySelector('.new-book');
 const form = document.querySelector('form');
 
